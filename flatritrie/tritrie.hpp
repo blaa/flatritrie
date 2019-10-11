af5b5a3408c60349dc924cb59ee7effc1dd55586
Tomasz bla Fortuna <bla@thera.be>
2019-10-11 14:11:51 +0200

    IPv6 support and required refactor.
    - std::stoi instead of atoi.
    - inet_pton instead of inet_aton
    - Drop boost dependency in tritrie/*
    - Some runtime exceptions instead of asserts to guard the interface.
    - IPv6 unittest.
    - Rewrite to work with int128_t.

--- flatritrie/tritrie.hpp
+++ flatritrie/tritrie.hpp
@@ -4,19 +4,37 @@
  * bla@thera.be, https://github.com/blaa/flatritrie
  */
 
-#ifndef _TRITRIE_H_
-#define _TRITRIE_H_
+#ifndef _BLA_TRITRIE_H_
+#define _BLA_TRITRIE_H_
 
 #include <iostream>
 #include <string>
 #include <vector>
-#include <boost/algorithm/string.hpp>
+#include <bitset>
+#include <cassert>
 
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
 namespace Tritrie {
+
+using uint128_t = unsigned __int128;
+
+/* Debug: support for printing unsigned __int128 */
+std::ostream &operator<<(std::ostream &os, const uint128_t &data) {
+    union {
+        uint128_t whole;
+        struct {
+            uint64_t low;
+            uint64_t high;
+        };
+    } broken;
+    broken.whole = data;
+    os << "H" << std::bitset<64>(broken.high);
+    os << "L" << std::bitset<64>(broken.low);
+    return os;
+}
 
 /* Common defaults for tritrie and flatritrie */
 constexpr int DEFAULT_BITS = 8;
@@ -27,8 +45,9 @@ constexpr int DEFAULT_BITS = 8;
 template<int BITS=DEFAULT_BITS, typename K=uint32_t, typename V=int32_t, V def=-1>
 class Tritrie {
 protected:
-    constexpr static int MASK_MAX = std::numeric_limits<K>::max();
-    constexpr static int BITS_TOTAL = std::numeric_limits<K>::digits;
+    /* NOTE: No support in numerical_limits for int128 */
+    constexpr static K MASK_MAX = (K)(-1);
+    constexpr static int BITS_TOTAL = (8 * sizeof(K));
     constexpr static int CHILDREN = (1<<BITS);
 
     struct Node {
@@ -64,29 +83,38 @@ protected:
         return cur->child[tri];
     }
 
-    void add_ip(K ip, int32_t mask, V value) {
+    void add_ip(K ip, int mask, V value) {
         int mask_left = mask;
         Node *cur = &this->root;
 
-        assert(mask >= this->last_mask);
+        if (mask < this->last_mask) {
+            std::cerr << "Inserting mask " << mask
+                      << " after mask " << this->last_mask << std::endl;
+            throw std::runtime_error("Invalid order of IP insertion to Tritrie");
+        }
+
+        assert(BITS_TOTAL > BITS);
         this->last_mask = mask;
 
-        while (mask_left >= BITS) {
+        while (mask_left >= BITS) { /* TODO: CONV to for */
+            /* Shave "BITS" most significant bits */
             const int tri = ip >> (BITS_TOTAL - BITS);
+            ip <<= BITS;
 
             cur = this->get_or_create(cur, tri);
 
             /* Next one! */
-            ip <<= BITS;
             mask_left -= BITS;
         }
 
+        /* Handle last level appropriately */
         if (mask_left) {
-            /* Handle last level appropriately */
+            /* Mask is not aligned and splits the Tritrie level */
 
             ip >>= (BITS_TOTAL - BITS);
 
             /*
+             * Possibilities for BITS=3 or 4:
              * mask_left   final_mask  BITS
              * 1           100            3
              * 2           110            3
@@ -97,11 +125,12 @@ protected:
              * etc.
              * (0xffffffff >> (BITS_TOTAL-mask_left)) << (BITS - mask_left)
              */
-            const int mask = (
+            const K mask = (
                 (MASK_MAX >> (BITS_TOTAL - mask_left)) << (BITS - mask_left)
             );
+            assert(mask != 0);
 
-            for (uint32_t tri = 0; tri < CHILDREN; tri++) {
+            for (int tri = 0; tri < CHILDREN; tri++) {
                 if ((tri & mask) == ip) {
                     /* Insert here */
                     auto lvl = this->get_or_create(cur, tri);
@@ -111,12 +140,6 @@ protected:
         } else {
             /* After using whole mask, the IP should be 0 */
             assert(ip == 0);
-
-            /* No duplicates at the same level are acceptable */
-            if (cur->value != -1) {
-                std::cout << "New ID " << value << " collided with existing "
-                          << cur->value << std::endl;
-            }
             cur->value = value;
         }
     }
@@ -133,6 +156,45 @@ protected:
         }
     }
 
+    /**
+     * Decompose string form of an IP to numerical address and mask.
+     * Sets mask to -1 if it's not given.
+     */
+    void ip_from_string(const std::string &addr_mask, K &ip_n, int &mask_n) const {
+        std::string addr;
+        size_t found = addr_mask.find("/");
+        if (found == std::string::npos) {
+            mask_n = -1;
+            addr = addr_mask;
+        } else {
+            addr = addr_mask.substr(0, found);
+            std::string mask_s = addr_mask.substr(found + 1, addr_mask.size());
+            mask_n = std::stoi(mask_s);
+        }
+
+        if constexpr (BITS_TOTAL == 32) {
+            in_addr ip_parsed;
+            int ret = inet_pton(AF_INET, addr.c_str(), &ip_parsed);
+            if (ret == 0)
+                throw std::runtime_error("Unable to parse IPv4 address");
+
+            ip_n = ntohl(ip_parsed.s_addr);
+        } else if constexpr (BITS_TOTAL == 128) {
+            in6_addr ip_parsed;
+            int ret = inet_pton(AF_INET6, addr.c_str(), &ip_parsed);
+            if (ret == 0)
+                throw std::runtime_error("Unable to parse IPv6 address");
+
+            /* Convert IPv6 to host order, so that bitshifts work ok */
+            ip_n = 0;
+            for (int i=0; i<16; i++) {
+                ip_n |= ((K)ip_parsed.s6_addr[i]) << (120 - 8*i);
+            }
+        } else {
+            throw std::runtime_error("IP Address of unknown lenght");
+        }
+    }
+
     /* Don't copy. */
     Tritrie(const Tritrie &tritrie);
 
@@ -142,31 +204,29 @@ public:
         this->release(&this->root);
     }
 
-    void add(const std::string &addr, V value) {
-        std::vector<std::string> addr_mask;
-        boost::split(addr_mask, addr, boost::is_any_of("/"));
-        assert(addr_mask.size() == 2);
+    void add(const std::string addr_mask, V value) {
+        K ip;
+        int mask;
+        this->ip_from_string(addr_mask, ip, mask);
+        if (mask == -1) {
+            throw std::runtime_error("Address without a mask");
+        }
 
-        const int mask = atoi(addr_mask[1].c_str());
-        in_addr ip_parsed;
-        int ret = inet_aton(addr_mask[0].c_str(), &ip_parsed);
-        assert(ret != 0);
         assert(mask >= 1 && mask <= BITS_TOTAL);
-        if (ret == 0 || mask < 1 || mask > BITS_TOTAL)
+        if (mask < 1 || mask > BITS_TOTAL)
             throw std::exception();
-
-        uint32_t ip_network = ntohl(ip_parsed.s_addr);
-        this->add_ip(ip_network, mask, value);
+        this->add_ip(ip, mask, value);
     }
 
     V query_string(const std::string &addr) const {
-        in_addr ip_parsed;
-        int ret = inet_aton(addr.c_str(), &ip_parsed);
-        assert(ret != 0);
-        if (ret == 0)
-            throw std::exception();
-        uint32_t ip_network = ntohl(ip_parsed.s_addr);
-        return this->query(ip_network);
+        K ip;
+        int mask;
+        this->ip_from_string(addr, ip, mask);
+        if (mask != -1 && mask != BITS_TOTAL) {
+            throw std::runtime_error("Query with partial mask.");
+        }
+
+        return this->query(ip);
     }
 
     V query(K ip) const {
@@ -195,4 +255,5 @@ public:
 };
 
 };
+
 #endif
--- unit_tests.cpp
+++ unit_tests.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <iostream>
+#include <cstdint>
 #include "trie.hpp"
 #include "tritrie.hpp"
 #include "flatritrie.hpp"
@@ -13,7 +14,7 @@
 
 namespace Test {
 
-const std::vector<std::pair<std::string, int>> data = {
+const std::vector<std::pair<std::string, int>> data_v4 = {
     /* Address, ID */
     {"255.0.0.0/8", 0},
     {"255.255.0.0/16", 1},
@@ -30,7 +31,7 @@ const std::vector<std::pair<std::string, int>> data = {
     {"10.255.0.3/32", 3},
 };
 
-const std::vector<std::pair<std::string, int>> testcases = {
+const std::vector<std::pair<std::string, int>> testcases_v4 = {
     {"10.255.0.0", 2},
     {"10.255.1.0", 2},
     {"10.255.255.255", 2},
@@ -60,15 +61,67 @@ const std::vector<std::pair<std::string, int>> testcases = {
     {"95.175.151.254", 5},
 };
 
-template<typename T>
-int runner(T &algo) {
+const std::vector<std::pair<std::string, int>> data_v6 = {
+    /* Address, ID */
+    {"2001:200::/32", 1},
+    {"2001:200:4000::/38", 2},
+
+    {"2001:200:4490::/44", 3},
+    {"2001:200:4488::/45", 4},
+
+    {"2001:470:0:285::/64", 23},
+
+    {"2001:470:0:284::2000/115", 11},
+    {"2001:470:0:284::1000/116", 10},
+
+    {"2001:470:0:284::2/127", 22},
+    {"2001:470:0:284::/128", 20},
+    {"2001:470:0:284::1/128", 21},
+
+    {"2001:470:1f0b:a9:9dc3:6ed8:e819:f89a/128", 40},
+
+};
+
+const std::vector<std::pair<std::string, int>> testcases_v6 = {
+    {"2001:200::/128", 1},
+    {"2001:200::10/128", 1},
+    {"2001:200:1:2:3:4:5:6", 1},
+
+    {"2001:200:4000::1", 2},
+
+    {"2001:200:4000:ffff:ffff:ffff:ffff:ffff", 2},
+    {"2001:200:4490::1", 3},
+    {"2001:200:4488::1", 4},
+
+    {"2001:470:0:284::1000", 10},
+    {"2001:470:0:284::1fff", 10},
+    {"2001:470:0:284::999", -1},
+
+    {"2001:470:0:284::2000", 11},
+    {"2001:470:0:284::", 20},
+    {"2001:470:0:284::1", 21},
+    {"2001:470:0:284::2", 22},
+    {"2001:470:0:284::3", 22},
+
+    {"2001:470:0:285::", 23},
+    {"2001:470:0:285:a:b:c:d", 23},
+
+    {"2001:470:1f0b:a9:9dc3:6ed8:e819:f89a", 40},
+    {"2001:470:1f0b:a9:9dc3:6ed8:e819:f89b", -1},
+    {"2001:470:1f0b:a9:9dc3:6ed8:e819:f899", -1},
+    {"2002:470:1f0b:a9:9dc3:6ed8:e819:f89a", -1},
+};
+
+
+template<typename T, typename K>
+int runner(T &algo, K &testcases) {
     int successes = 0;
     int failures = 0;
-    for (auto &testcase: Test::testcases) {
+    for (auto &testcase: testcases) {
         int ret = algo.query_string(testcase.first);
         if (ret != testcase.second) {
             std::cout << "TEST FAIL " << testcase.first
-                      << " ret=" << ret << " != "
+                      << " returned " << ret << " should "
                       << testcase.second
                       << std::endl;
             failures += 1;
@@ -86,24 +139,24 @@ int runner(T &algo) {
 
 int testcase_map() {
     IPMap<> map;
-    for (auto &item: Test::data) {
+    for (auto &item: Test::data_v4) {
         map.add(item.first, item.second);
     }
 
     std::cout << "Map testcases" << std::endl;
-    int ret = Test::runner<>(map);
+    int ret = Test::runner<>(map, Test::testcases_v4);
     return ret;
 }
 
 int testcase_trie() {
     int ret;
     Trie trie;
-    for (auto &item: Test::data) {
+    for (auto &item: Test::data_v4) {
         trie.add(item.first, item.second);
     }
 
     std::cout << "Trie testcases" << std::endl;
-    ret = Test::runner<>(trie);
+    ret = Test::runner<>(trie, Test::testcases_v4);
 
     /* Flatrie */
     FlaTrie flatrie;
@@ -111,7 +164,7 @@ int testcase_trie() {
 
     // flatrie.show();
     std::cout << "Flatrie testcases" << std::endl;
-    ret += Test::runner<>(flatrie);
+    ret += Test::runner<>(flatrie, Test::testcases_v4);
 
     return ret;
 }
@@ -123,32 +176,57 @@ int testcase_tritrie() {
 
     std::cout << "Generating tritrie<" << BITS << ">" << std::endl;
     int id = 0;
-    for (auto &item: Test::data) {
+    for (auto &item: Test::data_v4) {
         tritrie.add(item.first, item.second);
         id++;
     }
 
     std::cout << "Testing tritrie<" << BITS << ">" << std::endl;
-    ret = Test::runner<>(tritrie);
+    ret = Test::runner<>(tritrie, Test::testcases_v4);
 
     /* FlaTritrie test */
     Tritrie::Flat<BITS> flatritrie;
     flatritrie.build(tritrie);
     std::cout << "Testing flatritrie<" << BITS << ">" << std::endl;
-    ret += Test::runner<>(flatritrie);
+    ret += Test::runner<>(flatritrie, Test::testcases_v4);
 
     /* Should build second time as well */
     flatritrie.build(tritrie);
+
+
+    /* Error handling */
+    try {
+        tritrie.add("8.8.8.8", 100); /* Throws exception */
+        std::cout << "Parsing error" << std::endl;
+        ret += 1;
+    } catch(std::runtime_error &re) {
+    }
+    return ret;
+}
+
+template<int BITS>
+int testcase_ipv6() {
+    int ret = 0;
+    Tritrie::Tritrie<BITS, Tritrie::uint128_t> tritrie;
+
+    std::cout << "Generating tritrie<" << BITS << "> for IPv6" << std::endl;
+    int id = 0;
+    for (auto &item: Test::data_v6) {
+        tritrie.add(item.first, item.second);
+        id++;
+    }
+
+    ret += Test::runner<>(tritrie, Test::testcases_v6);
 
     return ret;
 }
 
 int main() {
     int ret = 0;
+
     ret = testcase_map();
     ret += testcase_trie();
-
-    /* meh */
+    // meh
     ret += testcase_tritrie<1>();
     ret += testcase_tritrie<2>();
     ret += testcase_tritrie<3>();
@@ -157,6 +235,8 @@ int main() {
     ret += testcase_tritrie<6>();
     ret += testcase_tritrie<7>();
     ret += testcase_tritrie<8>();
+
+    testcase_ipv6<8>();
 
     return ret;
 }
